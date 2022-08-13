7820dc0fe158aba1e1d9b063f75ba4bab0486b6a
Roxana Pacho <pachuuu@live.com.ar>
2022-08-13 13:13:17 -0300

    modificacion de las rutas que el parametro estaba mal

--- src/componentes/Item/Item.js
+++ src/componentes/Item/Item.js
@@ -1,3 +1,35 @@
+
+// import './Item.css'
+// import { Link} from 'react-router-dom'
+
+// const Item = ({ id, name, price, img}) => {
+//   return (
+   
+//       <article className="CardItem">
+//       <header className="Header">
+//           <h2 className="ItemHeader">
+//               {name}
+//           </h2>
+//       </header>
+//       <picture>
+//           <img src={img} alt={name} />
+//       </picture>
+//       <section>
+//           <p className="Info">
+//               Precio: ${price}
+//           </p>
+//       </section>           
+//       <footer className='ItemFooter'>
+//           <Link to={`/detail/${id}`} className='Option'>Ver detalle</Link>
+//       </footer>
+//   </article>
+// )
+  
+// }
+
+
+// export default Item
+
 import React from 'react'
 import './Item.css'
 import { Link} from 'react-router-dom'
@@ -8,12 +40,11 @@ const Item = ({Productos, id}) => {
            <li className='card-li'>
                <h3 className='card-title'>{Productos.name}</h3>
                <img className='Item-img' src={Productos.img} alt={Productos.name}/>
-               <Link to={`/detail/${id}`} className='Option '>Ver detalle</Link>
-        
-           </li>
-    </div>       
-  )
-}
+               <Link to={`/detail/${Productos.id}`}>Ver detalle</Link>
+            </li>    
+               
+    </div>   
+)}
 
 
 export default Item
